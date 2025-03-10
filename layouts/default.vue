521a8e51e9df0b6b5594e1018ac683843b256ece
EslamKamel89 <elsamkamelforex@gmail.com>
2025-03-10 17:13:29 +0200

    enhance layout and styling by integrating Tailwind CSS and updating default layout structure

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,25 @@
 <template>
-  <div>
-    <nav>
-      <ul>
-        <li>
-          <NuxtLink to="/">Home </NuxtLink>
-        </li>
-        <li>
-          <NuxtLink to="/about">About </NuxtLink>
-        </li>
-      </ul>
-    </nav>
-    <slot />
+  <div class="container mx-auto px-2 max-w-3xl mt-5">
+    <header class="flex justify-between items-center">
+      <div>
+        <NuxtLink to="/" class="text-2xl font-medium p-2 hover:bg-gray-200"
+          >Eslam Kamel</NuxtLink
+        >
+      </div>
+      <nav>
+        <ul class="flex gap-x-2 font-mono">
+          <li>
+            <NuxtLink to="/">Home </NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/about">About </NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main class="p-2">
+      <slot />
+    </main>
   </div>
 </template>
 
